1d34ac9d3a6271753068156ae6d794d6b00d3159
Arjitha Balaji <34678027+Arjithab@users.noreply.github.com>
2025-05-26 19:20:13 -0400

    Update load_pdf.py

--- src/load_pdf.py
+++ src/load_pdf.py
@@ -1,8 +1,8 @@
 # Unstructured PDF loader
-from langchain_community.document_loaders import UnstructuredFileLoader
+from langchain_unstructured import UnstructuredLoader
 
 def extract_text_from_pdf(pdf_path):
-    loader = UnstructuredFileLoader(pdf_path)
+    loader = UnstructuredLoader(pdf_path)
     documents = loader.load()
     text = "\n".join([doc.page_content for doc in documents])
     return text
