676520103ecab1656128794b67dca30813e51889
Arjitha Balaji <34678027+Arjithab@users.noreply.github.com>
2025-05-26 19:15:51 -0400

    Update qa_pipeline.py

--- src/qa_pipeline.py
+++ src/qa_pipeline.py
@@ -1,4 +1,7 @@
 # RAG QA pipeline
+
+#For local testing
+import torch
 from langchain.chains import RetrievalQA
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain_community.llms import HuggingFacePipeline
@@ -8,11 +11,14 @@ def setup_qa_chain(vectorstore, model_name="microsoft/BioGPT-Large"):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
+    device = 0 if torch.backends.mps.is_available() else -1
+    
     # Define pipeline
     pipe = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
+        device=device,
         max_new_tokens=256,
         do_sample=True,
         temperature=0.7,
@@ -24,7 +30,7 @@ def setup_qa_chain(vectorstore, model_name="microsoft/BioGPT-Large"):
     llm = HuggingFacePipeline(pipeline=pipe)
 
     # Setup retrieval-augmented QA
-    retriever = vectorstore.as_retriever()
-    qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 5}
+    qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, return_source_documents=True)
 
     return qa_chain
