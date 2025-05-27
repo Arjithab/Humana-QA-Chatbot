d74ed9bff6edb07c033895c286b86ed9b1d66650
Arjitha Balaji <34678027+Arjithab@users.noreply.github.com>
2025-05-27 01:16:33 -0400

    Update load_pdf.py

--- src/load_pdf.py
+++ src/load_pdf.py
@@ -1,5 +1,8 @@
 # Unstructured PDF loader
 from langchain_community.document_loaders import UnstructuredFileLoader
+import warnings
+
+warnings.filterwarnings("ignore", category=UserWarning, module="transformers.pytorch_utils")
 
 def extract_text_from_pdf(pdf_path):
     loader = UnstructuredFileLoader(pdf_path)
